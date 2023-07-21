e2da561f10a6c0f5d7ffabbdc25b1f216335415e
1871vinayak <1871vinayak@gmail.com>
2023-07-21 22:45:11 +0530

    Add To-Do UI

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -8,6 +8,25 @@
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
+    @Published var title = ""
+    @Published var duedate = Date()
+    @Published var showAlert = false
+    
     init() {}
     
+    func save(){
+        
+    }
+    
+    var canSave: Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard duedate >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        
+        return true
+    }
 }
